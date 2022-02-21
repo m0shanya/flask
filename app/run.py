bc3a963805bea84326b21aa93e2c77aa8b3d6911
m0shanya <funnytimka@gmail.com>
2022-02-21 22:03:09 +0300

    add cw 21.02

---
+++ app/run.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, render_template
+import os
+import redis
+
+app = Flask(__name__)
+
+redis_host = os.getenv("REDIS_HOST", "localhost")
+redis_client = redis.Redis(host=redis_host, port=6379, db=0)
+
+
+@app.route("/")
+def hello_world():
+    if request.method == "POST":
+        value = None
+        if "key" in request.values:
+            value = request.values.get("key")
+            redis_client.set("key", value)
+    else:
+        value = redis_client.get("key")
+    return render_template("index.html", value=value)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
